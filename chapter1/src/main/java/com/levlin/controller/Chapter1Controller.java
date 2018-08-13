826741116bb43805156a47ec04b0aa3c5c0c1032
ultimately <1018633688@qq.com>
2018-08-14 00:22:25 +0800

    第一次提交！

---
+++ chapter1/src/main/java/com/levlin/controller/Chapter1Controller.java
@@ -0,0 +1,46 @@
+package com.levlin.controller;
+
+import com.levlin.annotation.CheckReqNo;
+import com.levlin.req.BaseResponse;
+import com.levlin.req.StatusEnum;
+import com.levlin.util.NULLBody;
+import com.levlin.util.RedisContent;
+import com.levlin.util.RedisContentReq;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/chapter1")
+@RestController
+public class Chapter1Controller {
+
+    private static final Logger log = LoggerFactory.getLogger(Chapter1Controller.class);
+
+
+    @Value("${my1.name}")
+    private String my1Name;
+
+
+    @CheckReqNo
+    @RequestMapping(value = "/createRedisContent",method = RequestMethod.POST)
+    @ResponseBody
+    public BaseResponse<NULLBody> createRedisContent(@RequestBody RedisContentReq redisContentReq){
+        BaseResponse<NULLBody> response = new BaseResponse<NULLBody>() ;
+        RedisContent rediscontent = new RedisContent() ;
+        try {
+            //CommonUtil.setLogValueModelToModel(redisContentReq,rediscontent);
+            //rediscontentMapper.insertSelective(rediscontent) ;
+            response.setReqNo(redisContentReq.getReqNo());
+            response.setCode(StatusEnum.SUCCESS.getCode());
+            response.setMessage(StatusEnum.SUCCESS.getMessage());
+        }catch (Exception e){
+            log.error("system error",e);
+            response.setReqNo(response.getReqNo());
+            response.setCode(StatusEnum.FAIL.getCode());
+            response.setMessage(StatusEnum.FAIL.getMessage());
+        }
+        return response ;
+    }
+
+}
